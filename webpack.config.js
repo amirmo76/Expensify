5b931543aec3c6dee37f8aad49ed4ba672ef01ad
amirmo76 <amir.mohseni7697@yahoo.com>
2019-03-24 20:08:36 +0430

    devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = env => {
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, "public"),
+      path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js"
     },
     module: {
@@ -23,8 +23,6 @@ module.exports = env => {
             {
               loader: MiniCssExtractPlugin.loader,
               options: {
-                // you can specify a publicPath here
-                // by default it use publicPath in webpackOptions.output
                 publicPath: "../"
               }
             },
@@ -52,7 +50,8 @@ module.exports = env => {
     devtool: isproduction ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: "/dist/"
     }
   };
 };
